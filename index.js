d3e44950520f8bc3bc466faa11c72cfbc289bc2f
Brian Lambert <brianlambert2408@gmail.com>
2022-09-14 10:11:33 +1000

    Completed tasks

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1=2, num2=31, num3 = 24, num4=5;
+
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,20,14,13,12,11);
